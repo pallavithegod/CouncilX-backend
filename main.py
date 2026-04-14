33d0d541b9fde5151ee2b32884d9c5705961014b
Garvit Arora <garvit.university@gmail.com>
2026-04-14 13:54:30 +0530

    Editing Ports

--- main.py
+++ main.py
@@ -126,4 +126,5 @@ async def health():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    port = int(os.getenv("PORT", 8080))
+    uvicorn.run(app, host="0.0.0.0", port=port)
