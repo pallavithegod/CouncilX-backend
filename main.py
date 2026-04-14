f9fe7853c177f7af1ccaa4994ad828b6fa9ab82a
Garvit Arora <garvit.university@gmail.com>
2026-04-14 14:37:37 +0530

    Updates

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-app = FastAPI(title="AI Parliament Backend")
+app = FastAPI(title="Council X Backend")
 
 app.add_middleware(
     CORSMiddleware,
